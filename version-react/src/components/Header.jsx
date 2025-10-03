e5c16dfc26adcf3f934dd803c0f63f53dbeeecd6
c-dal <chlo04.dls@gmail.com>
2025-10-03 15:11:08 +0200

    Début de react fait le 2 oct, chargement de 10 films différents avec l API

---
+++ version-react/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Navbar from './Navbar';
+
+function Header() {
+    return (
+        <header className="site-header" role="banner">
+            <Navbar />
+        </header>
+    )
+}
+
+export default Header;
